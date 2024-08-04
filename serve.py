0eb37065258d0ce5289320055be629a0d6b8ac2d
tadeo <tade.g.17@gmail.com>
2024-08-04 18:17:22 +0200

    add to test in railway

--- serve.py
+++ serve.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from typing import List
+from typing import List, Union
 
 from fastapi import FastAPI
 from langchain_openai import ChatOpenAI
@@ -10,12 +10,14 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.tools.retriever import create_retriever_tool
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain import hub
-from langchain.agents import create_openai_functions_agent
-from langchain.agents import AgentExecutor
+from langchain.agents import AgentExecutor, create_openai_tools_agent
 from langchain.pydantic_v1 import BaseModel, Field
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from langserve import add_routes
 from langserve import RemoteRunnable
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers import StrOutputParser
+
 
 
 # Web loader
@@ -40,12 +42,15 @@ retriever_tool = create_retriever_tool(
 search = TavilySearchResults()
 tools = [retriever_tool, search]
 
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are an assistant for question-answering tasks that answers questions about Promtior."),
+    MessagesPlaceholder("chat_history", optional=True),
+    ("human", "{input} {agent_scratchpad}"),
+])
 
-prompt = hub.pull("hwchase17/openai-functions-agent")
 llm = ChatOpenAI( model="gpt-3.5-turbo", temperature=0)
-agent = create_openai_functions_agent(llm, tools, prompt)
+agent = create_openai_tools_agent(llm, tools,prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-
 
 app = FastAPI(
   title="LangChain Server",
@@ -55,19 +60,23 @@ app = FastAPI(
 
 class Input(BaseModel):
     input: str
-    chat_history: List[BaseMessage] = Field(
+    chat_history: List[HumanMessage | AIMessage | SystemMessage] = Field(
         ...,
-        extra={"widget": {"type": "chat", "input": "location"}},
+        description="The chat messages representing the current conversation.",
     )
-
 
 class Output(BaseModel):
     output: str
 
+playground_compatible_agent_executor = (
+    agent_executor | (lambda x: x["output"])
+).with_types(input_type=Input, output_type=Output)
+
 add_routes(
     app,
-    agent_executor.with_types(input_type=Input, output_type=Output),
+    playground_compatible_agent_executor,
     path="/promtior_chatbot",
+    playground_type="chat"
 )
 
 if __name__ == "__main__":
