f140a6f580790ffd2fb83b59af8b167ebbe15684
tadeo <tade.g.17@gmail.com>
2024-08-04 12:31:08 +0200

    update serve file

--- serve.py
+++ serve.py
@@ -73,6 +73,4 @@ add_routes(
 if __name__ == "__main__":
     import uvicorn
 
-    host = os.getenv("HOST", "0.0.0.0")
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host=host, port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
